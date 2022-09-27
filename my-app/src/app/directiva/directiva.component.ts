1d80faac2e48db65712c946d194a688eb726e7ac
onejulian <julian.correa@pulzo.com>
2022-09-27 11:53:31 -0500

    :sparkles: add component directiva

--- my-app/src/app/directiva/directiva.component.ts
+++ my-app/src/app/directiva/directiva.component.ts
@@ -5,6 +5,8 @@ interface Producto{
   stock:number;
   fabricante:string;
   fechaVence:Date;
+  esImportante:boolean;
+  textoGrande:boolean;
 }
 
 @Component({
@@ -13,6 +15,10 @@ interface Producto{
   styleUrls: ['./directiva.component.scss']
 })
 export class DirectivaComponent implements OnInit {
+
+  mostrarCuadrado: boolean = false;
+
+  pestana: string = ''
 
   cargando: boolean = true
 
@@ -23,25 +29,33 @@ export class DirectivaComponent implements OnInit {
       nombre : 'arroz',
       stock : 20,
       fabricante : 'Roa',
-      fechaVence : new Date('11/11/2032')
+      fechaVence : new Date('11/11/2032'),
+      esImportante: true,
+      textoGrande: false
     },
     {
       nombre : 'trigo',
       stock : 80,
       fabricante : 'Haz de oro',
-      fechaVence : new Date('12/1/2032')
+      fechaVence : new Date('12/1/2032'),
+      esImportante: false,
+      textoGrande: true
     },
     {
       nombre : 'avena',
       stock : 40,
       fabricante : 'Quacker',
-      fechaVence : new Date('03/12/2032')
+      fechaVence : new Date('03/12/2032'),
+      esImportante: true,
+      textoGrande: true
     },
     {
       nombre : 'cebada',
       stock : 25,
       fabricante : 'Polar',
-      fechaVence : new Date('04/13/2032')
+      fechaVence : new Date('04/13/2032'),
+      esImportante: false,
+      textoGrande: false
     }
   ]
 
@@ -55,6 +69,16 @@ export class DirectivaComponent implements OnInit {
 
   alternar(){
     this.cargando = !this.cargando
+  }
+
+  cambiarPestana(pestana: string)
+  {
+    this.pestana = pestana;
+  }
+
+  alternarFondo()
+  {
+    this.mostrarCuadrado = !this.mostrarCuadrado
   }
 
 }
