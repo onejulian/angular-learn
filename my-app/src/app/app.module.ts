07ab9bba600a3ed17d71816fc220b0ab769a9772
onejulian <julian.correa@pulzo.com>
2022-09-27 16:11:46 -0500

    :sparkles: Add component ejemplo

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { TitulosComponent } from './titulos/titulos.component';
 import { FormsModule } from '@angular/forms';
 import { DirectivaComponent } from './directiva/directiva.component';
+import { EjemploComponent } from './ejemplo/ejemplo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitulosComponent,
     DirectivaComponent,
+    EjemploComponent,
   ],
   imports: [
     BrowserModule,
